c57093d0fdf20e0608dd87e1bcbed706618326aa
V-Karch <luna.karch05@gmail.com>
2024-10-12 19:07:36 -0400

    NSPFile: added barebones implementation of NSPFile

---
+++ src/main/java/com/nsptools/Model/NSPFile.java
@@ -0,0 +1,32 @@
+package com.nsptools.Model;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class NSPFile {
+    private final String filename;
+    private final File file;
+
+    public NSPFile(String filename) throws FileNotFoundException {
+        this.filename = filename;
+        this.file = new File(filename);
+
+        if (!validateFile(this.file)) {
+            throw new FileNotFoundException("File " + filename + " could not be found.");
+        }
+
+    }
+
+    public File getFile() {
+        return this.file;
+    }
+
+    @Override
+    public String toString() {
+        return this.filename;
+    }
+
+    public static boolean validateFile(File file) {
+        return file.exists() && !file.isDirectory();
+    }
+}
