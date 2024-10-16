5e792c6a4bd1b5cdbdbdfc16270b1575e6f09d72
V-Karch <luna.karch05@gmail.com>
2024-10-16 15:42:05 -0400

    NSPSplitter: added JavaDocs

--- src/main/java/com/nsptools/Model/NSPSpliter.java
+++ src/main/java/com/nsptools/Model/NSPSpliter.java
@@ -8,10 +8,24 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.FileNotFoundException;
 
+/**
+ * Handles the splitting of NSP files into smaller parts.
+ * This class creates split files in a separate output directory.
+ *
+ * @author V-Karch
+ */
 public class NSPSpliter {
     private final NSPFile nspfile;
     private final File outputDir;
 
+    /**
+     * Constructs an NSPSpliter object with the specified filename.
+     * Verifies that the file meets the minimum size requirement and prepares the output directory.
+     *
+     * @param filename the name of the NSP file to be split
+     * @throws FileNotFoundException if the specified file does not exist or is a directory
+     * @throws IllegalArgumentException if the file size is less than the required size of 4000 MB
+     */
     public NSPSpliter(String filename) throws FileNotFoundException, IllegalArgumentException {
         this.nspfile = new NSPFile(filename);
 
@@ -27,6 +41,12 @@ public class NSPSpliter {
         }
     }
 
+    /**
+     * Generates a new filename for each part of the split file.
+     *
+     * @param partNumber the part number to append to the filename
+     * @return the full path of the new file part
+     */
     private String getNewFileName(int partNumber) {
         String fileName = this.nspfile.getFile().getName();
         int dotIndex = fileName.lastIndexOf('.');
@@ -35,8 +55,12 @@ public class NSPSpliter {
         return new File(outputDir, baseName + "_part_" + partNumber + extension).getPath();
     }
 
+    /**
+     * Splits the NSP file into smaller parts, each with a maximum size of 4000 MB.
+     * The split files are written to the output directory.
+     */
     public void split() {
-        long partSize = 4000L * 1024 * 1024; // 3.9 gigabytes in bytes
+        long partSize = 4000L * 1024 * 1024; // 4 gigabytes in bytes
         byte[] buffer = new byte[4 * 1024 * 1024]; // 4 MB buffer size
         int partNumber = 1;
 
@@ -70,6 +94,12 @@ public class NSPSpliter {
         }
     }
 
+    /**
+     * Validates that the NSP file is larger than 4000 MB.
+     *
+     * @param nspfile the NSP file to validate
+     * @return true if the file size is greater than 4000 MB, false otherwise
+     */
     public static boolean validateFileSize(NSPFile nspfile) {
         return nspfile.getSizeMB() > 4000; // Ensure that the file is larger than 4GB
     }
